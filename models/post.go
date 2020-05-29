47a966322e787d21a126a1947081c35d7955e246
EvanXzj <2686465170@qq.com>
2020-05-29 13:36:38 +0800

    Post Model

--- models/post.go
+++ models/post.go
@@ -11,3 +11,16 @@ type Post struct {
 	UserID    int
 	CreatedAt time.Time
 }
+
+// FormatCreatedAt format createdAt time
+func (p *Post) FormatCreatedAt() string {
+	return p.CreatedAt.Format("Jan 2, 2006 at 3:04pm")
+}
+
+// User get the user who wrote the post
+func (p *Post) User() (user User) {
+	user = User{}
+	Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = ?", post.UserID).
+		Scan(&user.ID, &user.UUID, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
