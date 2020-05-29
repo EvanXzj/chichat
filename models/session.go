f66bf1c6694bb8d3676751b51f95d01eba908d40
EvanXzj <2686465170@qq.com>
2020-05-29 11:37:27 +0800

    session model

--- models/session.go
+++ models/session.go
@@ -10,3 +10,45 @@ type Session struct {
 	UserID    string
 	CreatedAt time.Time
 }
+
+// Check if session is valid in the database
+func (session *Session) Check() (valid bool, err error) {
+	err = Db.QueryRow("SELECT id, uuid, email, user_id, created_at FROM sessions WHERE uuid = ?;", session.UUID).
+		Scan(&session.Id, &session.UUID, &session.Email, &session.UserId, &session.CreatedAt)
+	if err != nil {
+		valid = false
+		return
+	}
+	if session.ID != 0 {
+		valid = true
+	}
+	return
+}
+
+// Delete session from database
+func (session *Session) Delete() (err error) {
+	statement := "delete from sessions where uuid = ?;"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(session.UUID)
+	return
+}
+
+// User get the user info from the session
+func (session *Session) User() (user User, err error) {
+	user = User{}
+	err = Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = ?", session.UserId).
+		Scan(&user.Id, &user.UUID, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
+
+// DeleteAllSessions delete all sessions from database
+func DeleteAllSessions() error {
+	statement := "delete from sessions;"
+	_, err := Db.Exec(statement)
+	return err
+}
